48d3bb40d8fc43d6fffe974c5bb5e49ea3d29b9f
Marin Mestrovic <mmestrov2000@gmail.com>
2026-03-19 16:48:21 +0100

    Assign chat responder to data stack

--- amplify/functions/chatResponder/resource.ts
+++ amplify/functions/chatResponder/resource.ts
@@ -3,6 +3,7 @@ import { defineFunction } from '@aws-amplify/backend';
 export const chatResponder = defineFunction({
   name: 'chatResponder',
   entry: './handler.ts',
+  resourceGroupName: 'data',
   timeoutSeconds: 30,
   memoryMB: 256,
 });
